675b05d30d536c6405f09da357f81f2b45198dc2
Ariel Sempertegui <arielsempertegu11@gmail.com>
2025-10-27 17:15:43 +0100

    Punto de control 3: Refactorización

--- components/dropdown.tsx
+++ components/dropdown.tsx
@@ -22,27 +22,15 @@ export default function Dropdown({menuText}:{menuText: string}) {
                 onBlur={handleBlur}
             >
                 {menuText}
-                {isOpen ? 
-                    <svg
-                        className="rotate-180 w-4 h-4 transition-transform duration-200"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                    >
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
-                    </svg>
-                        :
-                    <svg
-                        className="w-4 h-4 transition-transform duration-200"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                    >
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
-                    </svg>
-                }
+                <svg
+                    className={`${isOpen ? 'rotate-180' : ''} w-4 h-4 transition-transform duration-200`}
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+                </svg>
             </button>
             {isOpen && (
                 <div className="absolute top-20 w-full bg-pink-950 p-4 rounded-lg z-10">
